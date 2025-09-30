b511197d7398ba2c5d0c61ef23a9f24953454ec8
yu_ri <5646234+yu_ri@user.noreply.gitee.com>
2025-09-30 09:58:18 +0800

    提交代码

--- app/pages/index/index.vue
+++ app/pages/index/index.vue
@@ -19,8 +19,8 @@ useSeoMeta({
   >
     <div class="hidden lg:block">
       <UColorModeImage
-        light="/images/light/line-1.svg"
         dark="/images/dark/line-1.svg"
+        light="/images/light/line-1.svg"
         class="absolute pointer-events-none pb-10 left-0 top-0 object-cover h-[650px]"
       />
     </div>
